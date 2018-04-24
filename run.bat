5634f086aacdf3e356ecb2ab306defe5e6f52f73
Aswani <aswani.keerthi@gmail.com>
2018-04-24 17:36:02 +0530

    changes done

--- run.bat
+++ run.bat
@@ -1,3 +1,7 @@
 SET FLASK_APP=run_cust_forecast.py
 flask run --host=0.0.0.0 --port=3001
 # flask run --host=0.0.0.0
+
+SET FLASK_APP=run_cust_dept_forecast.py
+flask run --host=0.0.0.0 --port=3001
+# flask run --host=0.0.0.0
--- run_cust_forecast.py
+++ run_cust_forecast.py
@@ -189,3 +189,153 @@ def customer_volume():
 	cust_forecast = forecast_df.to_json(orient='records', date_format = 'iso')[1:-1].replace('},{', '} {')
 	return cust_forecast
 	
+
+@app.route('/cust_dept_forecast', methods=['GET'])
+def customer_dept_volume():
+
+	#Generating the log file
+	logger = logging.getLogger(__name__)
+	logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s %(levelname)-8s %(message)s',
+                    datefmt='%a, %d %b %Y %H:%M:%S',
+                    filename='inc_vol_cust_dept_forecast_log.log',
+                    filemode='w')
+
+	INPUT_FILE_PATH = 'Customer_Data_Department_AMPM.xls'
+
+                            #==============================================================================================================#
+                            #		TREND AND VOLUME FORECAST BASED ON CLIENT FOR THE INCIDENT DATA USING ARIMA 	    				   #
+                            #==============================================================================================================#
+
+
+	if INPUT_FILE_PATH is not None:
+		filetype = INPUT_FILE_PATH.split('.')  #splitting the file name by '.' to get the file type
+		file_ext = filetype[-1]
+
+	#Getting the original column names and the renamed column names as per application from semantic mapping layer
+	orig_cols, rename_columns = semantic_columns('AMPM')
+
+	#Reading the incident file provided 
+	try:
+		incident_df = fp.input_file_read(INPUT_FILE_PATH, file_ext)
+		incident_df = incident_df[orig_cols]
+
+	except Exception as e:
+		logger.debug(e)
+		print(e)
+		exit(1)
+
+	# Extracting the respective column names based on application
+	try:
+		incident_df.columns = rename_columns
+		incident_df = incident_df[['Opened_at','Customer','Department']]
+	
+	except Exception as e:
+		logger.debug(e)
+		print(e)
+		logger.debug("There is an issue for taking the column names as per mapping of CLIENT TYPE")
+		logger.debug(incident_df.columns)
+		print("There is an issue for taking the column names as per mapping of CLIENT TYPE")
+		print(incident_df.columns)
+		exit(1)
+
+							# PREPROCESSING OF THE INCIDENT DATA INTO A SERIES WHICH CAN BE PASSED TO THE TIME-SERIES MODEL
+							# GETTING THE UNIQUE CUSTOMER VALUES
+
+	cust_list = list(incident_df['Customer'].value_counts().index)
+	dept_list = list(incident_df['Department'].value_counts().index)
+
+
+							#==============================================================================================================#
+               	 # LOOPING THROUGH EACH CUSTOMER, APPLY PRE PROCESSING, PASSING TO MODEL, FORECASTING FOR NEXT 3 MONTHS, STORING AS CSV FILE    #
+                            #==============================================================================================================#
+	for cust in cust_list:
+		for dept in dept_list:
+			try:
+				#calling the filter by group function to filter data by group
+				df_cust, msg = fp.filter_by_cust_dept(incident_df, cust, dept)
+				logger.info(msg)
+				print(df_cust.shape)
+				print(msg)
+
+				#calling the pre-processing function
+				data , msg  = fp.preprocessing_volume(df_cust)
+				logger.info(msg)
+				print(msg)
+
+			################################################################################################################################################
+			#												PASSING THE DATA TO THE ARIMA MODEL AND FINDING THE BEST CFG
+			################################################################################################################################################
+	
+				# evaluate parameters
+				p_values = range(0, 3)
+				d_values = range(0, 3)
+				q_values = range(0, 3)
+				warnings.filterwarnings("ignore")
+				print("processing for best cfg.....")
+				best_cfg = arima.evaluate_models(data.values, p_values, d_values, q_values)
+				print(best_cfg)
+
+				##Extracting the arima rmse, arima predictions on test data
+				arima_rmse, arima_preds, msg = arima.arima_validation(data, best_cfg)
+				logger.info(msg)
+
+				msg = 'RMSE for ARIMA model is %.3f' %arima_rmse
+				logger.debug(msg)
+				print(msg)
+
+				#Extracting the arima predictions
+				arima_forecast, msg = arima.arima_final_prediction(data, best_cfg)
+				logger.debug(msg)
+				print(msg)
+
+			################################################################################################################################################
+			#								SAVING THE RESULTS OF THE FORECAST TO THE DATABASE AS PER THE PATH MENTIONED IN COMMAND LINE
+			################################################################################################################################################
+	
+				#Extracting the last index date and using that date to get the next 3 months date
+				last_date = data.index[-1]
+				last_date = pd.Timestamp(last_date)
+
+				#Forecasting the next 3 months using the data for each customer with department
+				forecast_months = []	
+				for i in range(1, 4):
+					forecast_months.append(fp.last_day_of_month(last_date + relativedelta(months=i)))
+
+				#This step is convert actual data to pandas dataframe with columns Customer, Department, Month, Actuals and Forecast.
+				data_df = pd.DataFrame(data)
+				data_df['Month'] = data.index
+				data_df = data_df.reset_index(drop=True)
+				data_df.columns = ['Actuals','Month']
+				data_df['Customer'] = cust
+				data_df['Department'] = dept
+				data_df = data_df[['Customer','Department','Month','Actuals']]
+				length = len(data_df['Customer'])
+				data_df['Forecast'] = pd.Series(None, index=data_df.index)
+
+				#To the created dataframe of actual data, append the forecasted data also with customer name, department name, forecasted month and forecasted actuals.
+				for i in range(len(arima_forecast)):
+					data_df.loc[len(data_df)] = [cust,dept,forecast_months[i],None,arima_forecast[i]]
+
+				if 'forecast_df' not in locals():
+					forecast_df =  pd.DataFrame(columns=['Customer','Department','Month','Actuals','Forecast'])
+					forecast_df = forecast_df.append(data_df, ignore_index=True)
+
+				#In the else part it keeps adding each customer and department data to the forecast_df dataframe.
+				else:
+					forecast_df = forecast_df.append(data_df, ignore_index=True)
+			
+				msg = "The customer group {} and department group {} has been processed for forecasting".format(cust.upper(), dept.upper())
+				print(msg)
+			except:
+				msg = "The customer group {} and department group {} has not been processed and this has been skipped for forecasting".format(cust.upper(), dept.upper())
+				logger.debug(msg)
+				print(msg)
+				continue
+	
+
+	#SAVING THE FORECASTED DATA TO THE CSV FILE IN THE DETSINATION PATH MENTIONED
+	#forecast_df.to_json(OUTPUT_FILE_PATH)
+	forecast_df['Month'] = forecast_df['Month'].dt.strftime('%Y-%m-%d')
+	cust_dept_forecast = forecast_df.to_json(orient='records', date_format = 'iso')[1:-1].replace('},{', '} {')
+	return cust_dept_forecast
