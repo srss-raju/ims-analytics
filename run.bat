043d1a64dd21e26c7e0e3ed959dc083287788cb3
Aswani <aswani.keerthi@gmail.com>
2018-04-24 17:43:20 +0530

    made changes

--- run.bat
+++ run.bat
@@ -1,7 +1,3 @@
-SET FLASK_APP=run_cust_forecast.py
-flask run --host=0.0.0.0 --port=3001
-# flask run --host=0.0.0.0
-
-SET FLASK_APP=run_cust_dept_forecast.py
+SET FLASK_APP=run_volume_forecast.py
 flask run --host=0.0.0.0 --port=3001
 # flask run --host=0.0.0.0
